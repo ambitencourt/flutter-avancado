6cd6e14c09145a79d9895d2182b695da725bb82a
ambitencourt <adriano.mirandabitencourt@gmail.com>
2021-12-18 18:47:29 -0300

    OK

--- DICAS/flutter.dart
+++ DICAS/flutter.dart
@@ -1,2 +1,4 @@
 //classes aceitam guardar valores, enum não conseguem
 //Typedef atribui a um tipo de função um nome que você pode usar ao declarar campos e retornar tipos.
+//Para criar um data class(Modelo/Dados de classe) de forma fácil, utiliza-se a extensão para vscode
+//Dart Data Class Generator
---
+++ INTERFACE/interface.dart
@@ -0,0 +1,15 @@
+///
+///INTERFACE É MUITO ÚTIL PARA IMPLEMENTAR MÉTODOS E MODELOS NAS CLASSES
+///
+
+//utiliza-se a letra I no nome da classe para indicar criação de Interface
+//mas o recomendado não é usar o I, por conta do autocomplete do vscode.
+
+// abstract class IAppColors {
+//   Color get title;
+// }
+
+// class AppColors implements IAppColors {
+//   @override
+//   Color get title => const Color(0xFFE45851);
+// }
